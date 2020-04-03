1d1fd66a3f3bcadc87482e3d1d39b811c84d42ed
vidyashree9792 <vshree9792@gmail.com>
2020-04-03 21:16:30 +0530

    Vidya

---
+++ src/org/sam/Details.java
@@ -0,0 +1,11 @@
+package org.sam;
+
+public class Details {
+	public static void main(String[] args) {
+		for (int i = 0; i < 20; i++) {
+			if (i%2==1) {
+				System.out.println(i);
+			}
+		}
+	}
+}
